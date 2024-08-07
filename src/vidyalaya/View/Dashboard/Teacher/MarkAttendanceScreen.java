1a428d7a5c5443253741b450109aa9c3cac7ad88
Trishan Wagle <mailtotrishan@gmail.com>
2024-08-07 22:05:43 +0545

    feat: attendance dao

--- src/vidyalaya/View/Dashboard/Teacher/MarkAttendanceScreen.java
+++ src/vidyalaya/View/Dashboard/Teacher/MarkAttendanceScreen.java
@@ -59,7 +59,7 @@ public class MarkAttendanceScreen extends javax.swing.JFrame implements Runnable
         setLocationRelativeTo(null);
         setResizable(false);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+//        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         Utils.setFrameIcon(this, "/vidyalaya/Assets/logo.png");
 
