57427d390de25475a6db226bd7e8506da96ab7d2
Nishan Bista <nishanbista525@gmail.com>
2024-08-15 06:02:28 +0545

    Model: User added

---
+++ src/test/Model/UserData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vidyalaya.Model;
+
+/**
+ *
+ * @author trish
+ */
+public interface UserData {
+
+    public int getId();
+
+    public int getAdminId();
+
+    public String getName();
+
+    public String getEmail();
+}
